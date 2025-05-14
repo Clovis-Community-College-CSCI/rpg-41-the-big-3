c725adc8cca19e104771bc65f3486e1afc824952
dizon_khai <dizon_khai@mail.fresnostate.edu>
2025-05-13 20:01:59 -0700

    added map files need to implement

---
+++ map.cc
@@ -0,0 +1,57 @@
+#include "map.h"
+#include <ncurses.h>
+#include <cstdlib>
+
+GameMap::GameMap() : playerPos({width/2, height/2}) {
+	grid.resize(height, std::vector<char>(width, '.'));
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if (rand() % 10 == 0) grid[i][j] = 'T';
+			if (rand() % 15 == 0) grid[i][j] = '#';
+		}
+	}
+
+	grid [playerPos.second][playerPos.first] = '.';
+}
+
+void GameMap::draw(WINDOW* win) const {
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+			if (y == playerPos.second && x == playerPos.first) {
+				wattron(win, COLOR_PAIR(3));
+				mvwaddch(win, y+2, x+2, '@');
+				wattroff(win, COLOR_PAIR(3));
+			} else {
+				mvaddch(win, y+2, x+2, grid[y][x]);
+			}
+		}
+	}
+}
+
+void GameMap::drawWorldMap(WINDOW* win, int currentY, int currentX) const {
+	int mapHeight = 10;
+	int mapWidth = 30;
+	int startY = (getmaxy(win) - mapHeight) /2;
+	int startx = (getmaxx(win) - mapWidth) /2;
+
+	wattron(win, COLOR_PAIR(3));
+	box(win, 0, 0);
+	wattroff(win, COLOR_PAIR(3));
+
+	mvwprintw(win, startY + mapHeight/2, startX + mapWidth/2, "X");
+	mvwprintw(win, startY + mapHeight + 1, startX, "Current Postion: (%d,%d)", currentX, currentY);
+}
+
+bool GameMap::movePlayer(int dy, int dx) {
+	int newX = playerPos.first + dx;
+	int newY = playerPos.second + dy;
+
+	if (newX >= && newX < width && newY >= 0 && newY < height && grid[newY][newX] != '#') {
+		playerPos.first = newX;
+		playerPos.second = newY;
+		return true;
+	}
+	return false;
+}
+
---
+++ map.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <utility>
+
+class GameMap{
+private:
+	std::vector<std::vector<char>> grid;
+	std::pair<int, int> playerPos;
+	const int width = 30;
+	const int height = 20;
+
+public: 
+	GameMap();
+	void draw(WINDOW* win) const;
+	void draw(WINDOW* win, int currentY, int currentX) const;
+	bool movePlayer(int dy, int dx);
+	std::pair<<int, int> getPlayerPos() const {return playerPos; }
+};
+
