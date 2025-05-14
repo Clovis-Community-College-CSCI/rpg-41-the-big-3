a39bd4b5d2f276611cfa53a8fcf68480f385c2ce
Wei <102565@my.scccd.edu>
2025-05-14 13:51:07 -0700

    Moved main from .h to .cc

--- Bridgineer.cc
+++ Bridgineer.cc
@@ -4,6 +4,7 @@
 #include "BSTElement.h"
 #include "CircDLelement.h"
 #include "/public/read.h"
+#include "Bridgineer.h"
 #include <cstdlib>
 #include <ctime>
 
@@ -11,70 +12,78 @@
 using namespace bridges;
 using namespace std;
 
-class LL {
-
-  public:
-    int data;
-    LL* prev;
-    LL* next;
-
-    LL(int x) {
-        data = x;
-    }
-
-};
-
-class HasInitiative {
-    int speed;
-
-    void set_speed(int newSpeed) {
-        if (newSpeed >= 1 || newSpeed <= 40) {
-            speed = newSpeed;
-        } else {
-            cout << "ERROR!\n";
-            exit(1);
-        }
-    }
-};
-
-void dice() {
-    srand(time(0));
-    int rando = (rand() % 20) + 1;
-    cout << rando << endl;
-}
-
 int main() {
 
-    dice();
+	// create Bridges object
+	Bridges bridges(1, "ForgotName",
+	                "1362253532750");
 
-    // create Bridges object, set credentials
-    Bridges bridges(1, "ForgotName",
-                    "1362253532750");
+	//----------------------------------WALL------------------------------------------
+	CircDLelement<HasInitiative> *turn = nullptr;
+	vector<CircDLelement<HasInitiative>*> storage;
+	while (true) {
 
-    // set title, description
-    bridges.setTitle("A Simple  Binary Search Tree Example");
-    bridges.setDescription("This example illustrates a binary search tree built using BRIDGES");
+		string input = read("Enter Name: ");
+		if (input == "SNAPSHOT") break;
 
-    // we will create the tree manually in this example
+		int input2 = read("Enter Speed (between 1-40): ");
+		if (input2 < 0) break;
 
-    CircDLelement<string> *first = new CircDLelement<string>("#1", "#TEMP1");
-    CircDLelement<string> *secon = new CircDLelement<string>("#2", "#TEMP2");
-    CircDLelement<string> *third = new CircDLelement<string>("#3", "#TEMP3");
+		HasInitiative *entity = new HasInitiative(input, input2);
 
-    first->setNext(secon);
-    secon->setNext(third);
-    //secon->setPrev(first);
-    third->setNext(first);
-    //third->setPrev(secon);
+		string label = input + " - Initiative: " + to_string(entity->get_Ini());
 
-    //first->setPrev(third);
+		storage.push_back(new CircDLelement<HasInitiative>(*entity, label));
+
+		delete entity;
+
+	}
+
+//Sort by Initiative
+	sort(storage.begin(), storage.end(), [](CircDLelement<HasInitiative>* a, CircDLelement<HasInitiative>* b) {
+		return (a->getValue().get_Ini() > b->getValue().get_Ini());
+	}
+	    );
 
 
-    bridges.setDataStructure(first);
-    bridges.visualize();
-    delete first;
-    delete secon;
-    delete third;
-    return 0;
+	int sizee = storage.size();
+
+	for (int i = 0; i < sizee; i++) {
+		storage[i]->setNext(storage[(i + 1) % storage.size()]);
+		storage[i]->setPrev(storage[(((i - 1) + storage.size()) % storage.size())]);
+	}
+
+	for (int i = 0; i < sizee;) {
+		CircDLelement<HasInitiative>* current = storage[i];
+		//Change this part here for whatever needs to be removed.
+		if (current->getValue().get_speed() == 39) {
+
+			CircDLelement<HasInitiative> * prev = current->getPrev();
+			CircDLelement<HasInitiative> * next = current->getNext();
+
+			if (current == next && current == prev) {
+				delete current;
+				storage.erase(storage.begin() + i);
+				break;
+			}
+			prev->setNext(next);
+			next->setPrev(prev);
+			delete current;
+			sizee--;
+			storage.erase(storage.begin() + i);
+		} else {
+			i++;
+		}
+	}
+	if (!storage.empty()) {
+		turn = storage.at(0);
+
+		bridges.setDataStructure(turn);
+		bridges.visualize();
+		for (auto x : storage) {
+			//  cout << x->getValue().initiative << "\n";
+			delete x;
+		}
+	}
+	return 0;
 }
-
